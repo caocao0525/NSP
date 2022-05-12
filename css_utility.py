e86d5b955e6b5a9caf3d5183e126e1faf541e9a2
caocao0525 <seohyun.lee.physics@gmail.com>
2022-05-12 12:19:23 +0900

    linux

--- css_utility.py
+++ css_utility.py
@@ -8,6 +8,7 @@
 
 
 import os
+import re
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -16,7 +17,7 @@ from motif_utils import seq2kmer
 
 # #### Preparing the .bed file list
 
-# In[8]:
+# In[2]:
 
 
 # file name reader
@@ -39,43 +40,64 @@ all_files=file_list_maker(path, bed_files)
 all_cell_pickles=file_list_maker(pickle_path, pickle_files)
 
 
+# In[3]:
+
+
+all_files[0]
+
+
+# In[4]:
+
+
+all_cell_pickles[0]
+
+
+# In[5]:
+
+
 # test file
 test_filename='../database/bed/unzipped/E017_15_coreMarks_stateno.bed'
 
 
-# In[5]:
+# In[6]:
 
 
 state_dict={1:"A", 2:"B", 3:"C", 4:"D", 5:"E",6:"F",7:"G",8:"H" ,
                 9:"I" ,10:"J",11:"K", 12:"L", 13:"M", 14:"N", 15:"O"}
 
 
-# In[6]:
+# In[7]:
 
 
 css_name=['TssA','TssAFlnk','TxFlnk','Tx','TxWk','EnhG','Enh','ZNF/Rpts',
           'Het','TssBiv','BivFlnk','EnhBiv','ReprPC','ReprPcWk','Quies']
 
 
-# In[7]:
+# In[8]:
 
 
 css_dict=dict(zip(list(state_dict.values()), css_name))  # css_dict={"A":"TssA", "B":"TssAFlnk", ... }
 
 
-# In[8]:
+# In[9]:
 
 
 css_color_dict={'TssA':(219, 57, 50), 'TssAFlnk': (222, 87, 54), 'TxFlnk': (107, 187, 87),
                 'Tx': (57, 124, 72), 'TxWk': (48, 98, 58), 'EnhG': (197, 213, 80), 'Enh': (245, 196, 98),
                 'ZNF/Rpts': (129, 194, 169), 'Het': (137,143,189), 'TssBiv': (192, 98, 95), 
                 'BivFlnk': (223, 156, 127), 'EnhBiv': (188, 182, 115), 'ReprPC': (147, 149, 153),
-                'ReprPCWk': (200, 202, 203), 'Quies': (240, 240, 240)}
+                'ReprPCWk': (200, 202, 203), 'Quies': (240, 240, 240)}  # 255,255,255 was white -> bright gray 
+
+
+# In[41]:
+
+
+css_name_col_dict=dict(zip(css_name,state_col_dict.values()))
 
 
 # #### Function to convert RGB into decimal RGB
 
-# In[9]:
+# In[10]:
 
 
 def colors2color_dec(css_color_dict):
@@ -87,13 +109,13 @@ def colors2color_dec(css_color_dict):
     return color_dec_list
 
 
-# In[10]:
+# In[11]:
 
 
 state_col_dict=dict(zip(list(state_dict.values()),colors2color_dec(css_color_dict)))
 
 
-# In[11]:
+# In[12]:
 
 
 state_col_255_dict=dict(zip(list(state_dict.values()),list(css_color_dict.values())))
@@ -101,7 +123,7 @@ state_col_255_dict=dict(zip(list(state_dict.values()),list(css_color_dict.values
 
 # #### Function to create pickle file (dataframe, expanded version) for an individual cell
 
-# In[12]:
+# In[13]:
 
 
 # create a pickle for a cell-wise dataframe
@@ -122,7 +144,7 @@ def total_df2pickle(total_df_list):
 
 # #### Functions to make .bed to dataframe
 
-# In[13]:
+# In[14]:
 
 
 # create dataframe from bed file
@@ -143,7 +165,7 @@ def bed2df_as_is(filename):
     return df
 
 
-# In[14]:
+# In[15]:
 
 
 def bed2df_expanded(filename):
@@ -167,7 +189,7 @@ def bed2df_expanded(filename):
     return df 
 
 
-# In[15]:
+# In[16]:
 
 
 def total_df_maker(all_files):
@@ -186,7 +208,7 @@ def total_df_maker(all_files):
 # 
 # CSS here refers Chromatin state sequence
 
-# In[16]:
+# In[17]:
 
 
 def numchr(df):
@@ -194,7 +216,7 @@ def numchr(df):
     return df["chromosome"].nunique()    
 
 
-# In[17]:
+# In[18]:
 
 
 # create a large piece of string of the whole state_seq_full 
@@ -212,7 +234,7 @@ def df2css_allchr(df):
 
 # #### Create CSS chromosome-wise
 
-# In[18]:
+# In[19]:
 
 
 # first, learn where one chromosome ends in the df
@@ -246,9 +268,34 @@ def df2chr_index(df):
     return chr_index
 
 
+# #### Create df cut by each chromosome
+
+# In[20]:
+
+
+def df2chr_df(df):
+   
+    """Create a list of dataframes, each of which containing 
+    the the whole expanded type of dataframe per chromosome"""
+    
+    start=0
+    df_chr_list=[]
+    chr_index=df2chr_index(df)
+    
+    for index in chr_index:
+        df_chr=df[start:index+1] # note that python [i:j] means from i to j-1
+        chr_name=df["chromosome"].iloc[start] # string, such as chr1, chr2, ...
+        df_name='df_'+chr_name  # the chromosome-wise data stored like df_chr1, df_chr2, ...
+        locals()[df_name]=df_chr # make a string into a variable name
+        df_chr_list.append(df_chr)
+        start=index+1
+    
+    return df_chr_list   # elm is the df of each chromosome
+
+
 # #### Create CSS chromosome-wise, string only
 
-# In[19]:
+# In[21]:
 
 
 # create a list of dataframes, each of which contains the name of chromosome and chromosome-wise string of state_seq_full
@@ -257,24 +304,24 @@ def df2chr_index(df):
 def df2css_chr(df):
    
     """Create a list of dataframes, each of which containing 
-    the chromosome name and the state_seq_full per chromosome"""
+    the chromosome name and the state_seq_full per chromosome (2 columns)"""
     
     start=0
-    df_chr_list=[]
+    df2col_chr_list=[]
     chr_index=df2chr_index(df)
     
     for index in chr_index:
         df_chr=df[["chromosome","state_seq_full"]][start:index+1] # note that python [i:j] means from i to j-1
         chr_name=df["chromosome"].iloc[start] # string, such as chr1, chr2, ...
-        df_name='df_'+chr_name  # the chromosome-wise data stored like df_chr1, df_chr2, ...
-        locals()[df_name]=df_chr # make a string into a variable name
-        df_chr_list.append(df_chr)
+        df2col_name='df2col_'+chr_name  # the chromosome-wise data stored like df2col_chr1, df2col_chr2, ...
+        locals()[df2col_name]=df_chr # make a string into a variable name
+        df2col_chr_list.append(df_chr)
         start=index+1
     
-    return df_chr_list    
+    return df2col_chr_list    
 
 
-# In[20]:
+# In[22]:
 
 
 def df2css_chr_str(df):
@@ -284,11 +331,11 @@ def df2css_chr_str(df):
     chr_index=df2chr_index(df)  
     chr_index_num=len(chr_index) 
 
-    df_chr_list=df2css_chr(df)  # contains a list of df: chromosome name, state_seq_full (2-column datafame)
+    df2col_chr_list=df2css_chr(df)  # contains a list of df: chromosome name, state_seq_full (2-column datafame)
     chr_css_list=[]
 
     for num in range(chr_index_num): 
-        css_full_list=df_chr_list[num]["state_seq_full"].tolist()  # extract the state_seq_full only and make it a list
+        css_full_list=df2col_chr_list[num]["state_seq_full"].tolist()  # extract the state_seq_full only and make it a list
         css_full_to_str=''.join([elm for elm in css_full_list]) # make it a long string of all-connected state_seq_full (chromosome-wise)
         chr_css_list.append(css_full_to_str)
     return chr_css_list
@@ -300,7 +347,7 @@ def df2css_chr_str(df):
 # The variable of the above list is now called chr_css_list.<br>
 # Following functions will analyze the statistics of the each strings.
 
-# In[21]:
+# In[23]:
 
 
 def css_list2count(df, chr_css_list):
@@ -323,7 +370,39 @@ def css_list2count(df, chr_css_list):
     return count_all
 
 
+# In[37]:
 
+
+def draw_count_barplot_incl15(count_all, chr_no):
+    
+    """ Draw a bar plot (chromatin state vs. count) per chromosome
+    input(1) table of 'count_all' which is created by the function css_list2count(df, chr_css_list) 
+    input(2) chromosome name in string, e.g.) 'chr1', 'chr2', ... 
+    output: bar plot of the all chromatin state count (including 15th state)"""
+
+    count_all_renamed=count_all.rename(index=css_dict)
+    color_dec=colors2color_dec(css_color_dict)
+    count_all_renamed.loc[:,chr_no].plot.bar(rot=45, color=color_dec)
+    ax0=ax0.set_ylabel("Counts", fontsize=14)
+
+
+# In[38]:
+
+
+def draw_count_barplot_wo15(count_all, chr_no):
+    
+    """ Draw a bar plot (chromatin state vs. count) per chromosome
+    input(1) table of 'count_all' which is created by the function css_list2count(df, chr_css_list) 
+    input(2) chromosome name in string, e.g.) 'chr1', 'chr2', ... 
+    output: bar plot of the all chromatin state count except for 15th state"""
+
+    count_all_renamed=count_all.rename(index=css_dict)
+    color_dec=colors2color_dec(css_color_dict)
+    ax0=count_all_renamed.loc[:,chr_no][:-1].plot.bar(rot=45, color=color_dec)
+    ax0.set_ylabel("Counts", fontsize=14)  
+
+
+# In[36]:
 
 
 def colored_css_str(sub_str):
@@ -339,7 +418,150 @@ def colored_css_str(sub_str):
     return print("\033[1m"+col_str+"\033[0;0m") 
 
 
+# #### css pattern analysis without 15th state (state **O**)
+# 
+# 1. create a list of a css without 15th state, the element of which is connected (df2inbetweeen_lst)
+# 2. create a whole list of css without 15th state, using a all-chromosome df (df2wo15list)
+# 3. calculate the length of each element of the generated list, and analyze the statistics
 
+# In[29]:
+
+
+def df2inbetweeen_lst(df):
+    lst=[]
+    df_wo_o=df[df["state"]!=15]   #remove the 15th state from the css
+    css_df=df_wo_o["state_seq_full"]
+    str_elm=css_df.iloc[0]  # the very first elm
+    for i in range(1, len(css_df)):
+        # check the index first
+        cid=css_df.index[i] #init=1
+        pid=css_df.index[i-1] # init=0
+        ssf=css_df
+        if (cid-pid)!=1: # if the index is separated (not a succeeding numbers)
+            lst.append(str_elm)
+            str_elm=ssf.iloc[i]
+        else:            # if encountered a consecutive index
+            str_elm+=ssf.iloc[i] # attach the next str to the previous str
+            if i==len(css_df)-1:   # treat the final line
+                lst.append(str_elm)
+    return lst
+
+
+# In[30]:
+
+
+def df2wo15list(df):
+    total_lst=[]
+    df_chr_list=df2chr_df(df)   # a list, elm of which is the df of each chromosome
+    for df_chr in df_chr_list:   # for each chromosome, create a grand list by adding up the whole
+        lst_chr=df2inbetweeen_lst(df_chr)
+        total_lst+=lst_chr
+    return total_lst   # total_lst here consists of the connected-patterns betweeen 15th state
+
+
+# In[31]:
+
+
+def css_elm_stat(total_lst):# graph of the length distribution 
+    len_lst=[]              # total_lst here consists of the connected-patterns betweeen 15th state
+    for elm in total_lst:
+        assert type(elm)==str, "element type is not string"
+        len_lst.append(len(elm))
+    print("total count: ", len(total_lst))
+    print("max length: ", max(len_lst))
+    print("min length: ", min(len_lst))
+    print("average length: ",np.mean(len_lst))
+    fig =plt.figure(figsize=(6,4))
+    plt.hist(len_lst, bins=20, log=True, color="teal", edgecolor="white")
+    plt.xlabel("length of chromatin state pattern", fontsize=14)
+    plt.ylabel("Count", fontsize=14)
+
+
+# In[33]:
+
+
+def lst2let_compose(total_lst):# graph of the number of letter composed for a pattern
+    letter_cnt=[]              # total_lst here consists of the connected-patterns betweeen 15th state
+    for word in total_lst:
+        chk_let=word[0]
+        num_let=1
+        for let in word:
+            if let!=chk_let:
+                num_let+=1
+                chk_let=let
+        letter_cnt.append(num_let)
+    print("total count: ", len(letter_cnt))
+    print("max composition: ", max(letter_cnt))
+    print("min composition: ", min(letter_cnt))
+    print("average composition: ", np.mean(letter_cnt))
+    fig =plt.figure(figsize=(6,4))
+    plt.hist(cnt_lst, bins=20, log=True, color="orange", edgecolor="white")
+    plt.xlabel("number of state in a composition", fontsize=14)
+    plt.ylabel("Count", fontsize=14)
+
+
+# In[ ]:
+
+
+# def lst2solo_compose(total_lst):
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# ### class test ...should I make a class?
+
+# In[28]:
+
+
+# class bed2df_cls:
+    
+#     def __init__(self, fname):
+        
+#         self.fname=fname
+        
+#         df_raw=pd.read_csv(fname, sep='\t', lineterminator='\n', 
+#                            header=None, low_memory=False)
+#         df=df_raw.rename(columns={0:"chromosome",1:"start",2:"end",3:"state"})
+#         df=df[:-1] # remove the end row: it displayed the cell id and track no.
+#         df["start"]=pd.to_numeric(df["start"])
+#         df["end"]=pd.to_numeric(df["end"])
+#         df["length"]=df["end"]-df["start"]
+#         df["unit"]=(df["length"]/100).astype(int)
+        
+#         state_dict={1:"A", 2:"B", 3:"C", 4:"D", 5:"E",6:"F",7:"G",8:"H" ,
+#                     9:"I" ,10:"J",11:"K", 12:"L", 13:"M", 14:"N", 15:"O"}
+        
+#         df["state"]=pd.to_numeric(df["state"])
+#         df["state_seq"]=df["state"].map(state_dict)
+#         df["state_seq_full"]=df["unit"]*df["state_seq"]
+        
+#         self.df=df
+#         self.df_len=len(df)
+#         self.numchr=df["chromosome"].nunique()
+        
+#         print(".df : dataframe \n.df_len : length of dataframe \n.numchr : no. of chromosome")
+        
+     
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
 
 
 
