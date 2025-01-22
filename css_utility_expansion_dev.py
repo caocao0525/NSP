acb84ab8a8c0b518b8b09f043f1a36e928d014fa
caocao0525 <seohyun.lee.physics@gmail.com>
2025-01-22 16:16:46 +0900

    update

--- css_utility_expansion_dev.py
+++ css_utility_expansion_dev.py
@@ -4,15 +4,17 @@
 # # CSS utility
 # 
 # Functions that can be exploited for data pre-processing and downstream analysis
+# 
+# ChromBERT has been expanded to include support for IHEC data
 
-# In[207]:
+# In[210]:
 
 
 # ### To convert the file into .py
 # !jupyter nbconvert --to script css_utility_expansion_dev.ipynb
 
 
-# In[105]:
+# In[2]:
 
 
 import os
@@ -534,6 +536,38 @@ def df2unitcss(df):
 # # test passed
 
 
+# In[ ]:
+
+
+# #### To save css_unit from df_pickled in bulk
+# df_path = "../database/df_pickled"
+# output_path = "../database/css_unit_pickled"
+
+# for file_name in os.listdir(df_path):
+#     file_path = os.path.join(df_path, file_name)  # Full path to the file
+#     with open(file_path, "rb") as f:
+#         df = pickle.load(f)
+        
+#         # Extract file_id only for files starting with 'IHEC'
+#         if file_name[:4] == 'IHEC':
+#             file_id = file_name[:14]
+#         else:
+#             continue  # Skip files that do not match the condition
+
+#         # Process the dataframe and save the output
+#         unit_css = df2unitcss(df)
+#         output_file_name = os.path.join(output_path, f"{file_id}_unitcss.pkl")
+#         with open(output_file_name, "wb") as g:
+#             pickle.dump(unit_css, g)
+# ## this has been conducted, no need to execute again
+
+
+# In[ ]:
+
+
+
+
+
 # In[139]:
 
 
@@ -747,7 +781,7 @@ def save_TSS_by_loc(whole_gene_file, input_path="./",output_path="./",file_name=
 # # test passed
 
 
-# In[147]:
+# In[ ]:
 
 
 # Pretrain data preprocessing and storing
@@ -786,6 +820,45 @@ def save_css_by_cell_wo_continuous_15state(path_to_css_unit_pickled, output_path
     return 
 
 
+# In[2]:
+
+
+# [IHEC] Pretrain data preprocessing and storing (for 18-state version)
+
+# Preprocessing for removing continuous R state for pretrain dataset
+# 1-1. Save the CSS per cell, per chromosome
+def save_css_by_cell_wo_continuous_18state(path_to_css_unit_pickled, output_path,k=4):
+    # read files from css_unit_pickled
+    files=os.listdir(path_to_css_unit_pickled)
+    file_path_lst=[os.path.join(path_to_css_unit_pickled,file) for file in files]
+    for file_path in file_path_lst:
+        file_name=os.path.basename(file_path)
+        if file_name[:4] == 'IHEC':
+            file_id = file_name[:14]
+        else:
+            pass
+        # ##########################
+        # if str(file_id)=="E003":
+        #     break  # for test
+        # ##########################
+        with open(file_path,"rb") as f:
+            css=pickle.load(f)
+        css_kmer=[]
+        for css_chr in css:
+            css_chr_kmer=seq2kmer(css_chr,k)
+            target_to_remove="R"*k   # get rid of the word with continuous 15th state "o"
+            css_chr_kmer_trim = css_chr_kmer.replace(target_to_remove, "")
+            # clean up extra spaces
+            css_chr_kmer_trim = ' '.join(css_chr_kmer_trim.split())
+            css_kmer.append(css_chr_kmer_trim)
+        output_file_name=os.path.join(output_path,file_id+"_unitcss_wo_all"+str(k)+"R_state.pkl")    
+        with open(output_file_name, "wb") as g:
+            pickle.dump(css_kmer, g)  # note that it is chromosome-wise list (each element corresponds to each chromosome)
+
+        print("trimmed css by cell saved: ",file_id)
+    return 
+
+
 # In[148]:
 
 
@@ -801,6 +874,43 @@ def kmerCSS_to_pretrain_data(path_to_kmer_css_unit_pickled,output_path):
         file_name=os.path.basename(file_path)
         if file_name[0] == 'E' and file_name[1:4].isdigit():
             file_id = file_name[:4]
+        else:
+            pass
+        # ##########################
+        # if str(file_id)=="E003":
+        #     break  # for test
+        # # ##########################
+        with open(file_path,"rb") as f:
+            css=pickle.load(f)
+
+        css_per_cell='\n'.join(css)   # join the chromosome by new line
+
+        css_all.append(css_per_cell)   
+
+    css_all_cell='\n'.join(css_all)  # join the cell by new line
+
+    output_name=os.path.join(output_path,"pretrain_genome_all.txt") 
+    with open(output_name, "w") as g:
+        g.write(css_all_cell)
+
+    return 
+
+
+# In[ ]:
+
+
+# Preprocessing for removing continuous O state for pretrain dataset
+# 2-2. Concatenate all the cells and create one .txt file (IHEC file version)
+# (Note. new line joining chromosome-wise and cell-wise)
+def kmerCSS_to_pretrain_data_ihec(path_to_kmer_css_unit_pickled,output_path):
+    files=os.listdir(path_to_kmer_css_unit_pickled)
+    file_path_lst=[os.path.join(path_to_kmer_css_unit_pickled,file) for file in files]
+
+    css_all=[]
+    for file_path in file_path_lst:
+        file_name=os.path.basename(file_path)
+        if file_name[:4] == 'IHEC':
+            file_id = file_name[:14]
         else:
             pass
         # ##########################
